746b727e16cea142ce38b19283f40996314460d2
Aidan Do <aidand@canva.com>
2024-12-28 18:33:33 +1100

    .

--- hello4.py
+++ hello4.py
@@ -1,15 +1,8 @@
 
 directory = "/Users/aidand/dev/hello-swe-bench/django"
 
-print(f"Running in directory: {directory}")
-
-print(f"Solving problem statement...")
-
 with open('problem_statement.txt', 'r') as f:
     problem_statement = f.read()
-
-print(f"Problem statement: {problem_statement}")
-
 
 instruction = (
     '<uploaded_files>\n'
