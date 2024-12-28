c2b74f0906bbb3e8f93f6dbf42f66aa74529f21a
Aidan Do <aidand@canva.com>
2024-12-28 18:30:40 +1100

    .

--- prepare_for_openhands.bash
+++ prepare_for_openhands.bash
@@ -1,3 +1,10 @@
-cd /Users/aidand/dev/hello-swe-bench
-zip -r django.zip django/ -x "django/env/*" "django/test.patch"
+# cd /Users/aidand/dev/hello-swe-bench
+# zip -r django.zip django/ -x "django/env/*" "django/test.patch"
 
+echo "Copying django to OpenHands workspace"
+
+cp -r django/ /Users/aidand/dev/OpenHands/workspace/django
+trash /Users/aidand/dev/OpenHands/workspace/django/env
+trash /Users/aidand/dev/OpenHands/workspace/django/test.patch
+
+echo -e "\033[32mDone\033[0m"
