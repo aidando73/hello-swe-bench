5c14b1c9cce9953c67f98fed28ac65ef081d1f02
Aidan Do <aidand@canva.com>
2024-12-28 16:10:27 +1100

    .

--- hello3.bash
+++ hello3.bash
@@ -1,6 +1,8 @@
 
-echo $1
+
 
 cd /Users/aidand/dev/hello-swe-bench/django
 
-./tests/runtests.py --verbosity 2 --settings=test_sqlite --parallel 1
+echo "Running command: ./tests/runtests.py --verbosity 2 --settings=test_sqlite --parallel 1 $1"
+
+./tests/runtests.py --verbosity 2 --settings=test_sqlite --parallel 1 $1
