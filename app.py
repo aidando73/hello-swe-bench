dc9bb4cb952a5be83de465b447d2101709fbee17
Aidan Do <aidand@canva.com>
2024-12-29 22:03:57 +1100

    .

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from litellm import completion
 
-completion(
-    model="gpt-4o",
+model = "fireworks_ai/accounts/fireworks/models/llama-v3p1-405b-instruct"
+# model = "gpt-4o"
+
+response = completion(
+    model=model,
     messages=[{"role": "user", "content": "Hello, how are you?"}],
 )
+
+print(response)
