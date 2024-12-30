1ad7ac42e65498bb53800b4ff8ba88a8eedddafc
Aidan Do <aidand@canva.com>
2024-12-31 09:22:26 +1100

    .

--- app10.py
+++ app10.py
@@ -2,12 +2,19 @@ import json
 import os
 from llama_stack_client import LlamaStackClient
 
-model = "fireworks_ai/accounts/fireworks/models/llama-v3p3-70b-instruct"
+# model = "fireworks_ai/accounts/fireworks/models/llama-v3p3-70b-instruct"
+# MODEL_ID = "meta-llama/Llama-3.1-405B-Instruct-FP8"
+MODEL_ID = "meta-llama/Llama-3.3-70B-Instruct"
 # model = "fireworks_ai/accounts/fireworks/models/llama-v3p1-405b-instruct"
 # model = "openai/accounts/fireworks/models/llama-v3p1-405b-instruct"
 # model = "anthropic/claude-3-5-sonnet-20240620"
 
 # git ls-tree -r --name-only HEAD
+
+if "3.2" in MODEL_ID or "3.3" in MODEL_ID:
+    tool_prompt_format = "python_list"
+else:
+    tool_prompt_format = "json"
 
 file_tree = os.popen("cd django && git ls-tree -r --name-only HEAD").read()
 
@@ -39,34 +46,34 @@ StrReplaceEditorTool = {
         "command": {
             "description": "The commands to run. Allowed options are: `view`, `create`, `str_replace`, `insert`, `undo_edit`.",
             "enum": ["view", "create", "str_replace", "insert", "undo_edit"],
-            "type": "string",
+            "param_type": "string",
             "required": True,
         },
         "path": {
             "description": "Absolute path to file or directory, e.g. `/workspace/file.py` or `/workspace`.",
-            "type": "string",
+            "param_type": "string",
             "required": True,
         },
         "file_text": {
             "description": "Required parameter of `create` command, with the content of the file to be created.",
-            "type": "string",
+            "param_type": "string",
         },
         "old_str": {
             "description": "Required parameter of `str_replace` command containing the string in `path` to replace.",
-            "type": "string",
+            "param_type": "string",
         },
         "new_str": {
             "description": "Optional parameter of `str_replace` command containing the new string (if not given, no string will be added). Required parameter of `insert` command containing the string to insert.",
-            "type": "string",
+            "param_type": "string",
         },
         "insert_line": {
             "description": "Required parameter of `insert` command. The `new_str` will be inserted AFTER the line `insert_line` of `path`.",
-            "type": "integer",
+            "param_type": "integer",
         },
         "view_range": {
             "description": "Optional parameter of `view` command when `path` points to a file. If none is given, the full file is shown. If provided, the file will be shown in the indicated line number range, e.g. [11, 12] will show lines 11 and 12. Indexing at 1 to start. Setting `[start_line, -1]` shows all lines from `start_line` to the end of the file.",
             "items": {"type": "integer"},
-            "type": "array",
+            "param_type": "array",
         },
     },
 }
@@ -99,190 +106,193 @@ messages = [
     }
 ]
 
+client = LlamaStackClient(base_url=f"http://localhost:{os.environ['LLAMA_STACK_PORT']}")
+
 for i in range(ITERATIONS):
-    response = completion(
-        model=model,
+    response = client.inference.chat_completion(
+        model_id=MODEL_ID,
         messages=messages,
         tools=[StrReplaceEditorTool],
     )
+    print(response)
 
-    message = response["choices"][0]["message"]
-    # print(message)
-    messages.append(message)
-    if message.get("tool_calls") != None:
-        function = message["tool_calls"][0]["function"]
-        id = message["tool_calls"][0]["id"]
-    else:
-        # Try to parse the response as a tool call
-        # In some cases, the response is not a tool call, but in the response
-        try:
-            function = json.loads(message["content"])
-            if (
-                function["type"] != "function"
-                or function["name"] != "str_replace_editor"
-            ):
-                function = None
-            else:
-                function = {
-                    "name": function["name"],
-                    "arguments": json.dumps(function["parameters"]),
-                }
-                import uuid
+#     message = response["choices"][0]["message"]
+#     # print(message)
+#     messages.append(message)
+#     if message.get("tool_calls") != None:
+#         function = message["tool_calls"][0]["function"]
+#         id = message["tool_calls"][0]["id"]
+#     else:
+#         # Try to parse the response as a tool call
+#         # In some cases, the response is not a tool call, but in the response
+#         try:
+#             function = json.loads(message["content"])
+#             if (
+#                 function["type"] != "function"
+#                 or function["name"] != "str_replace_editor"
+#             ):
+#                 function = None
+#             else:
+#                 function = {
+#                     "name": function["name"],
+#                     "arguments": json.dumps(function["parameters"]),
+#                 }
+#                 import uuid
 
-                id = uuid.uuid4()
-        except json.JSONDecodeError:
-            function = None
-        except Exception as e:
-            print(f"Could not parse tool call: {e}")
-            import traceback
+#                 id = uuid.uuid4()
+#         except json.JSONDecodeError:
+#             function = None
+#         except Exception as e:
+#             print(f"Could not parse tool call: {e}")
+#             import traceback
 
-            print(f"Could not parse tool call: {e}")
-            print("Stacktrace:")
-            print(traceback.format_exc())
+#             print(f"Could not parse tool call: {e}")
+#             print("Stacktrace:")
+#             print(traceback.format_exc())
 
-            function = None
+#             function = None
 
-    if function:
-        try:
-            if "content" in message and message["content"] != None:
-                print("\033[95m" + message["content"] + "\033[0m")
-            arguments = json.loads(function["arguments"])
-            print(
-                "\033[94m" + function["name"],
-                json.dumps(arguments, indent=2) + "\033[0m",
-            )
-            if arguments["command"] == "str_replace":
-                try:
-                    with open(f"django/{arguments['path']}", "r") as f:
-                        file_content = f.read()
-                    with open(f"django/{arguments['path']}", "w") as f:
-                        old_str = arguments["old_str"]
-                        new_str = arguments["new_str"]
-                        new_content = file_content.replace(old_str, new_str)
-                        f.write(new_content)
-                        messages.append(
-                            {
-                                "role": "tool",
-                                "name": function["name"],
-                                "tool_call_id": id,
-                                "content": f"Result: {new_content}",
-                            }
-                        )
-                except FileNotFoundError:
-                    print(f"File {arguments['path']} not found. Skipping...")
-            elif arguments["command"] == "insert":
-                try:
-                    with open(f"django/{arguments['path']}", "w") as f:
-                        line_number = arguments["insert_line"]
-                        lines = f.readlines()
-                        lines.insert(line_number, arguments["new_str"])
-                        f.writelines(lines)
-                        messages.append(
-                            {
-                                "role": "tool",
-                                "name": function["name"],
-                                "tool_call_id": id,
-                                "content": f"Result: {f.read()}",
-                            }
-                        )
-                except FileNotFoundError:
-                    print(f"File {arguments['path']} not found. Skipping...")
-                    messages.append(
-                        {
-                            "role": "tool",
-                            "name": function["name"],
-                            "tool_call_id": id,
-                            "content": f"Result: Error - File {arguments['path']} not found.",
-                        }
-                    )
-            elif arguments["command"] == "view":
-                try:
-                    with open(f"django/{arguments['path']}", "r") as f:
-                        file_content = f.read()
-                        messages.append(
-                            {
-                                "role": "tool",
-                                "name": function["name"],
-                                "tool_call_id": id,
-                                "content": f"Result: {file_content}",
-                            }
-                        )
-                except FileNotFoundError:
-                    print(f"File {arguments['path']} not found. Skipping...")
-                    messages.append(
-                        {
-                            "role": "tool",
-                            "name": function["name"],
-                            "tool_call_id": id,
-                            "content": f"Result: Error - File {arguments['path']} not found.",
-                        }
-                    )
-            elif arguments["command"] == "create":
-                try:
-                    with open(f"django/{arguments['path']}", "w") as f:
-                        f.write(arguments["file_text"])
-                        messages.append(
-                            {
-                                "role": "tool",
-                                "name": function["name"],
-                                "tool_call_id": id,
-                                "content": f"Result: {f.read()}",
-                            }
-                        )
-                except FileNotFoundError:
-                    print(f"File {arguments['path']} not found. Skipping...")
-                    messages.append(
-                        {
-                            "role": "tool",
-                            "name": function["name"],
-                            "tool_call_id": id,
-                            "content": f"Result: Error - File {arguments['path']} not found.",
-                        }
-                    )
-        except json.JSONDecodeError:
-            print("\033[91mInvalid JSON in tool call arguments.\033[0m")
-            messages.append(
-                {
-                    "role": "tool",
-                    "name": function["name"],
-                    "tool_call_id": id,
-                    "content": f"Result: Error - Invalid JSON in tool call arguments: {function['arguments']}",
-                }
-            )
-        except Exception as e:
-            print(f"Error - skipping: {e}")
-            import traceback
+#     if function:
+#         try:
+#             if "content" in message and message["content"] != None:
+#                 print("\033[95m" + message["content"] + "\033[0m")
+#             arguments = json.loads(function["arguments"])
+#             print(
+#                 "\033[94m" + function["name"],
+#                 json.dumps(arguments, indent=2) + "\033[0m",
+#             )
+#             if arguments["command"] == "str_replace":
+#                 try:
+#                     with open(f"django/{arguments['path']}", "r") as f:
+#                         file_content = f.read()
+#                     with open(f"django/{arguments['path']}", "w") as f:
+#                         old_str = arguments["old_str"]
+#                         new_str = arguments["new_str"]
+#                         new_content = file_content.replace(old_str, new_str)
+#                         f.write(new_content)
+#                         messages.append(
+#                             {
+#                                 "role": "tool",
+#                                 "name": function["name"],
+#                                 "tool_call_id": id,
+#                                 "content": f"Result: {new_content}",
+#                             }
+#                         )
+#                 except FileNotFoundError:
+#                     print(f"File {arguments['path']} not found. Skipping...")
+#             elif arguments["command"] == "insert":
+#                 try:
+#                     with open(f"django/{arguments['path']}", "w") as f:
+#                         line_number = arguments["insert_line"]
+#                         lines = f.readlines()
+#                         lines.insert(line_number, arguments["new_str"])
+#                         f.writelines(lines)
+#                         messages.append(
+#                             {
+#                                 "role": "tool",
+#                                 "name": function["name"],
+#                                 "tool_call_id": id,
+#                                 "content": f"Result: {f.read()}",
+#                             }
+#                         )
+#                 except FileNotFoundError:
+#                     print(f"File {arguments['path']} not found. Skipping...")
+#                     messages.append(
+#                         {
+#                             "role": "tool",
+#                             "name": function["name"],
+#                             "tool_call_id": id,
+#                             "content": f"Result: Error - File {arguments['path']} not found.",
+#                         }
+#                     )
+#             elif arguments["command"] == "view":
+#                 try:
+#                     with open(f"django/{arguments['path']}", "r") as f:
+#                         file_content = f.read()
+#                         messages.append(
+#                             {
+#                                 "role": "tool",
+#                                 "name": function["name"],
+#                                 "tool_call_id": id,
+#                                 "content": f"Result: {file_content}",
+#                             }
+#                         )
+#                 except FileNotFoundError:
+#                     print(f"File {arguments['path']} not found. Skipping...")
+#                     messages.append(
+#                         {
+#                             "role": "tool",
+#                             "name": function["name"],
+#                             "tool_call_id": id,
+#                             "content": f"Result: Error - File {arguments['path']} not found.",
+#                         }
+#                     )
+#             elif arguments["command"] == "create":
+#                 try:
+#                     with open(f"django/{arguments['path']}", "w") as f:
+#                         f.write(arguments["file_text"])
+#                         messages.append(
+#                             {
+#                                 "role": "tool",
+#                                 "name": function["name"],
+#                                 "tool_call_id": id,
+#                                 "content": f"Result: {f.read()}",
+#                             }
+#                         )
+#                 except FileNotFoundError:
+#                     print(f"File {arguments['path']} not found. Skipping...")
+#                     messages.append(
+#                         {
+#                             "role": "tool",
+#                             "name": function["name"],
+#                             "tool_call_id": id,
+#                             "content": f"Result: Error - File {arguments['path']} not found.",
+#                         }
+#                     )
+#         except json.JSONDecodeError:
+#             print("\033[91mInvalid JSON in tool call arguments.\033[0m")
+#             messages.append(
+#                 {
+#                     "role": "tool",
+#                     "name": function["name"],
+#                     "tool_call_id": id,
+#                     "content": f"Result: Error - Invalid JSON in tool call arguments: {function['arguments']}",
+#                 }
+#             )
+#         except Exception as e:
+#             print(f"Error - skipping: {e}")
+#             import traceback
 
-            print("\033[91m" + "".join(traceback.format_exc()) + "\033[0m")
-            messages.append(
-                {
-                    "role": "tool",
-                    "name": function["name"],
-                    "tool_call_id": id,
-                    "content": f"Result: Error - {e}",
-                }
-            )
+#             print("\033[91m" + "".join(traceback.format_exc()) + "\033[0m")
+#             messages.append(
+#                 {
+#                     "role": "tool",
+#                     "name": function["name"],
+#                     "tool_call_id": id,
+#                     "content": f"Result: Error - {e}",
+#                 }
+#             )
 
-    else:
-        print(message["content"])
-        # if "<done>" in message["content"]:
-        #     break
-    print(
-        f"Input tokens: {response['usage']['prompt_tokens']}",
-        f"Output tokens: {response['usage']['completion_tokens']}",
-    )
-    if "anthropic" in model:
-        import time
+#     else:
+#         print(message["content"])
+#         # if "<done>" in message["content"]:
+#         #     break
+#     print(
+#         f"Input tokens: {response['usage']['prompt_tokens']}",
+#         f"Output tokens: {response['usage']['completion_tokens']}",
+#     )
+#     if "anthropic" in model:
+#         import time
 
-        time.sleep(60)
+#         time.sleep(60)
 
-# print("Loop finished")
-with open("messages.json", "w") as f:
-    json.dump(
-        [
-            message.model_dump() if hasattr(message, "model_dump") else message
-            for message in messages
-        ],
-        f,
-        indent=2,
-    )
+# # print("Loop finished")
+# with open("messages.json", "w") as f:
+#     json.dump(
+#         [
+#             message.model_dump() if hasattr(message, "model_dump") else message
+#             for message in messages
+#         ],
+#         f,
+#         indent=2,
+#     )
