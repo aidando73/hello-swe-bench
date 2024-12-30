88459b7ec3d557ab1374c498d44da4e5d24fd134
Aidan Do <aidand@canva.com>
2024-12-30 15:37:18 +1100

    .

--- prepare_for_openhands.bash
+++ prepare_for_openhands.bash
@@ -6,5 +6,6 @@ echo "Copying django to OpenHands workspace"
 cp -r django/ /Users/aidand/dev/OpenHands/workspace/django
 trash /Users/aidand/dev/OpenHands/workspace/django/env
 trash /Users/aidand/dev/OpenHands/workspace/django/test.patch
+trash /Users/aidand/dev/OpenHands/workspace/django/django/gold.patch
 
 echo -e "\033[32mDone\033[0m"
