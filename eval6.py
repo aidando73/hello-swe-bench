1aeb845d49d632dd74dae5ea9710662f94beb004
Aidan Do <aidand@canva.com>
2024-12-31 17:00:22 +1100

    .

--- eval6.py
+++ eval6.py
@@ -10,6 +10,10 @@ df_django = df[df['repo'] == 'django/django']
 
 df_django = df_django[df_django['version'].str.contains('5.') | df_django['version'].str.contains('4.')].reset_index(drop=True)
 
+# Set initial instance index to 0
+with open('current_instance.txt', 'w') as f:
+    f.write(f"0,{df_django.iloc[0]['instance_id']}")
+
 for index, row in df_django.iterrows():
     os.system(f"python setup6.py")
     os.system(f"python validate6.py")
